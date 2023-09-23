a129711d886aa96e371327634edf246bc624cff7
Sean Tansey <sean.d.tansey@gmail.com>
2023-09-23 10:46:41 -0600

    main: init

---
+++ src/configs/menu.ts
@@ -0,0 +1,46 @@
+
+export interface MenuItem {
+  title: string
+  price: number
+  ingredients: string
+  img: string
+}
+
+export const menuItems: MenuItem[] = [
+  {
+    title: "Margherita",
+    price: 17,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+  {
+    title: "Pepperoni",
+    price: 19,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+  {
+    title: "Funguy",
+    price: 21,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+  {
+    title: "Margherita",
+    price: 17,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+  {
+    title: "Pepperoni",
+    price: 17,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+  {
+    title: "Funguy",
+    price: 17,
+    ingredients: "House Red - Mozzarella - Basil - Pecorino Ramono",
+    img: "square_marg.jpeg"
+  },
+]
